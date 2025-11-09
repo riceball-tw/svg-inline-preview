861007465d770d3b83aacfbac8ee09c28a7de5ef
riceball <42161586+riceball-tw@users.noreply.github.com>
2025-11-09 19:43:08 +0800

    feat: Add svg inline preview

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,75 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activate
+let decorations: vscode.TextEditorDecorationType[] = [];
 
-
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let debounceTimer: NodeJS.Timeout;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "svg-inline-preview" is now active!');
+    vscode.workspace.onDidChangeTextDocument(event => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor && event.document === editor.document) {
+            clearTimeout(debounceTimer);
+            debounceTimer = setTimeout(() => {
+                updateDecorations(editor);
+            }, 0);
+        }
+    }, null, context.subscriptions);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('svg-inline-preview.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from svg-inline-preview!');
-	});
+    vscode.window.onDidChangeActiveTextEditor(updateDecorations, null, context.subscriptions);
+    vscode.window.onDidChangeActiveColorTheme(updateAllDecorations, null, context.subscriptions);
 
-	context.subscriptions.push(disposable);
+    updateAllDecorations();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function updateAllDecorations() {
+    vscode.window.visibleTextEditors.forEach(editor => {
+        updateDecorations(editor);
+    });
+}
+
+function updateDecorations(editor?: vscode.TextEditor) {
+    if (!editor) {
+        return;
+    }
+
+    const document = editor.document;
+
+    decorations.forEach(decoration => {
+        decoration.dispose();
+    });
+    decorations = [];
+
+    const text = document.getText();
+    const svgRegex = /<svg\b[^>]*>([\s\S]*?)<\/svg>/gi;
+    let match;
+
+    while ((match = svgRegex.exec(text)) !== null) {
+        const startPos = document.positionAt(match.index);
+        const svgContent = match[0];
+
+        const backgroundColor = vscode.workspace.getConfiguration('svg-inline-preview').get('backgroundColor', '#F5F5F5');
+
+        const styledSvgContent = svgContent.replace(
+            /(<svg\b[^>]*>)/,
+            `$1<style>svg{background-color:${backgroundColor};}</style>`,
+        );
+
+        const encodedSVG = encodeURIComponent(styledSvgContent.replace(/\s+/g, ' '));
+        const dataUrl = `data:image/svg+xml;charset=utf-8,${encodedSVG}`;
+
+        const decorationType = vscode.window.createTextEditorDecorationType({
+            gutterIconPath: vscode.Uri.parse(dataUrl),
+            gutterIconSize: 'contain'
+        });
+        decorations.push(decorationType);
+
+        const range = new vscode.Range(startPos, startPos);
+        editor.setDecorations(decorationType, [{ range }]);
+    }
+}
+
+export function deactivate() {
+    decorations.forEach(decoration => {
+        decoration.dispose();
+    });
+}
